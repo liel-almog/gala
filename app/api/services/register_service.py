deec251411e1bfd18a8e939246240550282f42a7
Liel Almog <liel.almogg@gmail.com>
2024-08-18 09:14:55 +0300

    Refactor: change the register_service to use event and guest repo's

--- app/api/services/register_service.py
+++ app/api/services/register_service.py
@@ -10,32 +10,35 @@ from app.api.models.register_model import (
     Registration,
     UnRegistraion,
 )
-from app.api.services.event_service import CommonEventService, EventService
-from app.api.services.guest_service import CommonGuestService, GuestService
+from app.api.repositories.event_repository import CommonEventRepository, EventRepository
+from app.api.repositories.guest_repository import (
+    CommonGuestRepository,
+    GuestRepository,
+)
 from app.core.db import CommonMongoClient
 
 
 class RegisterService:
     _client: AsyncIOMotorClient
-    _event_service: EventService
-    _guest_service: GuestService
+    _event_repository: EventRepository
+    _guest_repository: GuestRepository
 
     def __init__(
         self,
         client: AsyncIOMotorClient,
-        event_service: EventService,
-        guest_service: GuestService,
+        event_repository: EventRepository,
+        guest_service: GuestRepository,
     ) -> None:
         self._client = client
-        self._event_service = event_service
-        self._guest_service = guest_service
+        self._event_repository = event_repository
+        self._guest_repository = guest_service
 
     async def register(self, registration: Registration):
-        event = await self._event_service.get_one_by_id(registration.event_id)
+        event = await self._event_repository.find_one_by_id(registration.event_id)
 
         # No need to fetch the guest if the event is not VIP
         if event.is_vip_event:
-            guest = await self._guest_service.get_one_by_id(registration.guest_id)
+            guest = await self._guest_repository.find_one_by_id(registration.guest_id)
 
             if event.is_vip_event and not guest.is_vip:
                 raise GuestNotVipException("Guest is not VIP")
@@ -45,10 +48,10 @@ class RegisterService:
                 basic_event = BasicRegistrationInfo(id=event.id, name=event.name)
                 basic_guest = BasicRegistrationInfo(id=guest.id, name=guest.name)
 
-                add_event_to_guest_task = self._guest_service.add_event_to_guest(
+                add_event_to_guest_task = self._guest_repository.add_event_to_guest(
                     guest_id=registration.guest_id, event_basic_info=basic_event
                 )
-                add_guest_to_event_task = self._event_service.add_guest_to_event(
+                add_guest_to_event_task = self._event_repository.add_guest_to_event(
                     event_id=registration.event_id, guest_basic_info=basic_guest
                 )
 
@@ -58,13 +61,13 @@ class RegisterService:
         async with await self._client.start_session() as session:
             async with session.start_transaction():
                 remove_event_from_guest = await (
-                    self._guest_service.remove_event_from_guest(
+                    self._guest_repository.remove_event_from_guest(
                         guest_id=unregister.guest_id, event_id=unregister.event_id
                     )
                 )
 
                 remove_guest_from_event = await (
-                    self._event_service.remove_guest_from_event(
+                    self._event_repository.remove_guest_from_event(
                         event_id=unregister.event_id, guest_id=unregister.guest_id
                     )
                 )
@@ -74,8 +77,8 @@ class RegisterService:
 
 def get_register_service(
     client: CommonMongoClient,
-    event_service: CommonEventService,
-    guest_service: CommonGuestService,
+    event_service: CommonEventRepository,
+    guest_service: CommonGuestRepository,
 ):
     return RegisterService(client, event_service, guest_service)
 
